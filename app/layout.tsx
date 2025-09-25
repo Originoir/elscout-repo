0f3ab0854953429fad782080580db0472b7e0237
Originoir <aryaaditama07@gmail.com>
2025-09-26 00:47:22 +0700

    fix: tailwind setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
+import "./globals.css";
+
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <html lang="en">
-        <body style={{ background: "#111", color: "white" }}>{children}</body>
-      </html>
-    );
-  }
-  
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-900 text-white">{children}</body>
+    </html>
+  );
+}
