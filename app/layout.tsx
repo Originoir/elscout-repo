72fa16c35f89edcec8bc82bf224762be718c2008
Originoir <aryaaditama07@gmail.com>
2025-09-30 10:16:20 +0700

    Add responsive layout globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-gray-900 text-white">
-        <div className="flex">
-          <Sidebar />
-          <div className="flex-1">{children}</div>
+        <div className="flex min-h-screen">
+          {/* Sidebar for larger screens */}
+          <div className="hidden md:block">
+            <Sidebar />
+          </div>
+
+          {/* Main content area */}
+          <div className="flex-1 flex flex-col">
+            <div className="md:hidden">
+              <Sidebar />
+            </div>
+
+            {/* Page content area */}
+            <main className="flex-1 p-4 sm:p-6 overflow-x-auto">{children}</main>
+          </div>
         </div>
       </body>
     </html>
